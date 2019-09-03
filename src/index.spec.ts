5d42ac56bc790684eaba2e3fa2299100ef286c84
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2019-09-04 02:47:32 +0300

    test: add some more tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,9 @@
+jest.mock('@google-cloud/bigquery');
+import {BigQuery} from '@google-cloud/bigquery';
 import {WinstonBigQuery} from './index';
 
 describe('WinstonBigQuery', () => {
-	describe('#constructior', () => {
+	describe('#constructor', () => {
 		it('should verify datasetId is set', () => {
 			const options = {tableId: 'tableId'} as any;
 			expect(() => new WinstonBigQuery(options)).toThrow(
@@ -21,6 +23,29 @@ describe('WinstonBigQuery', () => {
 			expect(() => new WinstonBigQuery(options)).toThrow(
 				/Missing required GOOGLE_APPLICATION_CREDENTIALS/
 			);
+		});
+	});
+
+	describe('#log', () => {
+		process.env['GOOGLE_APPLICATION_CREDENTIALS'] = 'whatever';
+		const logger = new WinstonBigQuery({
+			tableId: 'tableId',
+			datasetId: 'dataseId'
+		});
+
+		beforeEach(() => {
+			jest.resetAllMocks();
+		});
+
+		it('should call the right dataset in bigquery', () => {
+			logger.log({
+				message: 'this is a Hello World',
+				meta1: 1,
+				meta2: 'string',
+				meta3: {deepObj: 1}
+			});
+			const a = new BigQuery();
+			expect(logger.bigquery.dataset).toHaveBeenCalledWith('dataseId');
 		});
 	});
 });
