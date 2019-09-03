7bcee5909aa310190e3778f4902358e8edf908a9
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2019-09-04 00:28:48 +0300

    build: add semantic-release changelog generator

---
+++ release.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/release-notes-generator',
+		[
+			'@semantic-release/changelog',
+			{
+				changelogFile: 'docs/CHANGELOG.md'
+			}
+		],
+		'@semantic-release/git'
+	]
+};
